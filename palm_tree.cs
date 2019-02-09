ca910963c6b628335f214385e6853e6ef2cbc9e2
BeachBoy <47483206+dmohaha@users.noreply.github.com>
2019-02-09 12:29:22 -0800

    Update palm_tree.cs

--- palm_tree.cs
+++ palm_tree.cs
@@ -1,5 +1,6 @@
 Using Systems;
 Public Static Void Main {
   Console.Writreln("Hey, Palm Tree!  Here I come!");
-  // print out a sentence
+  // print out a sentence: 1st change
+  // and then exit: 2nd change
 }
