6ff96cf7faab15e0aa073585eb83b433ed52cfd7
BeachBoy <47483206+dmohaha@users.noreply.github.com>
2019-02-09 13:56:05 -0800

    Master Branch: update palm_tree.cs

--- palm_tree.cs
+++ palm_tree.cs
@@ -4,4 +4,7 @@ Public Static Void Main {
   // print out a sentence: (1st change)
   // and then exit: (2nd change)
   // Branch added: beach (3rd Change)
+  // 
+  // Beach branch: after Pull Request and Merge, file in Beach merged into Master Branch.
+  // Master branch keep moving ahead: i.e. file updated (4th change) 
 }
